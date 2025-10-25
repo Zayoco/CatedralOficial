76315e9e6185ec6991dc76efa94ef1df7c03475d
el-Marce <gallardocoriamarcelodaniel@gmail.com>
2025-10-25 06:12:02 -0400

    Sonidos de menu, music, control de volumen

---
+++ Assets/Scripts/SoundScripts/UISounds.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using FMODUnity;
+using UnityEngine.UI;
+using FMOD.Studio;
+using static UnityEngine.Rendering.DebugUI;
+
+public class UISounds : MonoBehaviour
+{
+    [SerializeField] EventReference clickEvent;
+
+    
+    private void Awake()
+    {
+        int managers = FindObjectsOfType<UISounds>().Length;
+        if (managers > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    private void Start()
+    {
+    
+    }
+    void Update()
+    {        
+        
+    }
+
+
+    public void PlayClickDos()
+    {
+        if (!clickEvent.IsNull)
+            RuntimeManager.PlayOneShot(clickEvent);
+    }
+
+    
+}
