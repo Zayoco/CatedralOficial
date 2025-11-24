3c41acb45e8276d383fab98a4cf243a0a0cc0382
Zayoco <calle.choque.deranzahid@usfx.bo>
2025-11-24 18:14:37 -0400

    Volvi
    osvaldo porfavorcito no crees mas botones solo cambia el sprite atte isacc

--- Assets/UI scripts/menu1.cs
+++ Assets/UI scripts/menu1.cs
@@ -9,7 +9,7 @@ public class menu1 : MonoBehaviour
     [SerializeField] int indice;
     [SerializeField] private GameObject PanelSalirEscritorio;
     [SerializeField] private GameObject PanelOpciones;
-    [SerializeField] private GameObject PanelCreditos;
+    [SerializeField] private GameObject PanelMainMenu;
 
     private void Awake()
     {
@@ -28,44 +28,46 @@ public class menu1 : MonoBehaviour
     {
         switch (indice)
         {
-            case 0:
+            case 0: //activar escena 1, patio
                 SceneManager.LoadScene("escena 1");
                 break;
-            case 1:
+            case 1: //activar escena 2, salon principal
                 SceneManager.LoadScene("escena 2");
                 break;
-            case 2:
+            case 2: //activar panel salir escritorio
                 PanelSalirEscritorio.SetActive(true);
                 PanelOpciones.SetActive(false);
 
                 break;
-            case 3:
+            case 3: //salir escritorio, SI
                 Debug.Log("Saliendo...");
                 Application.Quit();
                 #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
                 #endif
                 break;
-            case 4:
+            case 4: //salir escritorio, NO
                 PanelSalirEscritorio.SetActive(false);
                 PanelOpciones.SetActive(true);
                 break;
-            case 5:
+            case 5: //activar panel opciones
+                PanelMainMenu.SetActive(false);
                 PanelOpciones.SetActive(true);
                 Time.timeScale = 0f; 
 
                 break;
-            case 6:
-                PanelOpciones.SetActive(true);
+            case 6: //desactivar panel opciones
+                PanelMainMenu.SetActive(true);
+                PanelOpciones.SetActive(false);
                 Time.timeScale = 1f; 
                 break;
-            case 7:
+            case 7: //activar escena creditos
                 SceneManager.LoadScene("Creditos");
                 break;
-            case 8:
+            case 8: //activar escena menu principal
                 SceneManager.LoadScene("PrincipalMenu");
                 break;    
-            default:
+            default: //indice no valido
                 Debug.Log("indice no valido");
                 break;
         }
